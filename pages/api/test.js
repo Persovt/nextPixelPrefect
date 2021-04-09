07263a484086693ce26b289f4d543d09b7616828
Persovt <mza200339@gmail.com>
2021-04-09 16:45:06 +0300

    add api and fetch

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,18 @@
-const PNG = require("png-js");
+const mongoose = require("mongoose");
 
-export default (req, res) => {
-    
-  console.log(req.body.file)
-  res.status(200).json({ test: "123" });
+mongoose.connect(
+  "mongodb+srv://persovt:mi260376@cluster0.fmkwc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const imageSchema = new mongoose.Schema({
+  name: String,
+  url: String,
+});
+
+const db = mongoose.connection;
+
+
+export default async (req, res) => {
+  console.log(req.body)
+  res.status(200).json({})
 };
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,45 @@
 import Head from "next/head";
-import {createRef,useEffect} from 'react'
+import { createRef, useEffect } from "react";
 import { Form } from "react-bootstrap";
 
-const checkFild = (e) => {
- 
-  var blobFile = e.target.files[0]
-  var formData = new FormData();
-  formData.append("fileToUpload", blobFile);
-  fetch('api/test', {
-    method: 'POST',
-    headers: {'Content-Type':'multipart/form-data'},
-    body: formData
-  })
+function ImageUrl({ url, signature }) {
+  this.url = url;
+  this.name = signature;
 }
+const uploadObjOnServer = async (e) => {
+  console.log(await uploadImages(e))
+  await fetch("api/test", {
+    method: "POST",
+    body: JSON.stringify(await uploadImages(e)),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+const uploadImages = async (e) => {
+  const arr = [];
+  const images = e.target.files;
+  for (const image of images)
+    arr.push(new ImageObj(await uploadImageOnServer(image)));
+
+  return arr;
+};
+
+const uploadImageOnServer = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "BillingRestro");
+  return await fetch("	https://api.cloudinary.com/v1_1/dpcp8wmqa/image/upload", {
+    method: "POST",
+    body: formData,
+  }).then((res) => res.json());
+};
 export default function Home({ allPostsData }) {
-  let ref = createRef();
-  
   return (
     <>
       <Form>
         <Form.File
-          // ref={ref}
-          onChange={checkFild}
-          // onChange={checkFild}
+          multiple
+          onChange={uploadObjOnServer}
           id="custom-file"
           label="Custom file input"
           accept=".jpg, .jpeg, .png"
